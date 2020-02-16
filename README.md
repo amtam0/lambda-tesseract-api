a9da2b154f49ae2f1a592ab6d6fbc07ce6b29fb9
hazimora33d <snat2100@gmail.com>
2020-02-16 19:55:56 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # tesseract-lambda-layer
+
+
+## Fast setup
+
+clone the repo
+cd .../repo
+
+bash build_py37_pkgs.sh
+bash build_tesseract4.0.0
+
+Upload manually layers to Aws lambda layers
+Check env to python 3.7
+
+Create lambda function
+environment variable / memory / timeout / code
+
+Test lambda
+
+Create Api
+new method POST
+enable CORS
+
+Test Api if OK deploy
+
+Test Api in Postman
+
+Done !
+
+#### Check Medium Blog for all th steps in details
+
+# Credits
+
+[Ocr Layer] https://github.com/bweigel/aws-lambda-tesseract-layer
+[Python libraries to layers] https://github.com/tiivik/LambdaZipper 
+[python3.7 lambda] https://github.com/lambci/docker-lambda
--- build_tesseract4.0.0.sh
+++ build_tesseract4.0.0.sh
@@ -1,20 +1,21 @@
 # Download tessconfigs folder
 git clone https://github.com/tesseract-ocr/tessconfigs.git
-TYPE=-best
+#TYPE can be best, fast or <empty>
+TYPE=
 # Remove old layer if exists
 rm tesseract-layer.zip
 rm tesseract-layer-fast.zip
 rm tesseract-layer-best.zip
 # Build Docker image containing Tesseract
 set -e
-docker build -t tess_layer .
+docker build -t tess_layer -f Dockerfile-tess4 .
 # Copy Tesseract locally
 CONTAINER=$(docker run -d tess_layer false)
 docker cp $CONTAINER:/opt/build-dist layer
 docker rm $CONTAINER
 # Zip Tesseract
 cd layer/
-zip -r ../tesseract-layer$TYPE.zip .
+zip -r ../tesseract-layer-$TYPE.zip .
 # Clean
 cd ..
 rm -rf layer/
