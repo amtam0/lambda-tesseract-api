f71a5362e19324893c34c9ec61f14e0387c9c4e6
hazimora33d <snat2100@gmail.com>
2020-02-17 23:43:35 +0100

    add new files

--- build_tesseract4.sh
+++ build_tesseract4.sh
@@ -1,11 +1,5 @@
 # Download tessconfigs folder
 git clone https://github.com/tesseract-ocr/tessconfigs.git
-#TYPE can be best, fast or <empty>
-TYPE=
-# Remove old layer if exists
-rm tesseract-layer.zip
-rm tesseract-layer-fast.zip
-rm tesseract-layer-best.zip
 # Build Docker image containing Tesseract
 set -e
 docker build -t tess_layer -f Dockerfile-tess4 .
@@ -15,7 +9,7 @@ docker cp $CONTAINER:/opt/build-dist layer
 docker rm $CONTAINER
 # Zip Tesseract
 cd layer/
-zip -r ../tesseract-layer-$TYPE.zip .
+zip -r ../tesseract-layer.zip .
 # Clean
 cd ..
 rm -rf layer/
